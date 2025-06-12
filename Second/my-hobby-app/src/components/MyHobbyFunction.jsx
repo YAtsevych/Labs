8e0a950853207174c48af0ac38f8f8e93feee802
YAtsevych <148938520+YAtsevych@users.noreply.github.com>
2025-06-12 19:49:29 +0300

    Delete Second/my-hobby-app directory

--- Second/my-hobby-app/src/components/MyHobbyFunction.jsx
+++
@@ -1,9 +0,0 @@
-const MyHobbyFunction = () => {
-  return (
-    <h2>
-      Моє хоббі - програмувати на JavaScript. Це розвиває логічне мислення.
-      Зроблено за допомогою Функції!
-    </h2>
-  )
-}
-export default MyHobbyFunction
